72a9f62309a52174dada4d34c9257e2c679ab29f
HarryR2608 <43859223+HarryR2608@users.noreply.github.com>
2021-08-02 23:47:11 +0100

    Add user model for backend

---
+++ backend/models/user.model.js
@@ -0,0 +1,14 @@
+﻿const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let User = new Schema({
+  user_username: {
+    type: String,
+  },
+  user_password: {
+    type: String,
+  },
+  user_grids: [{ type: Schema.Types.ObjectId, ref: "Grid" }],
+});
+
+module.exports = mongoose.model("User", User);
