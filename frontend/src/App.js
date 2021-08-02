90197d82ef2e779db206150733ba39024a6561c9
HarryR2608 <43859223+HarryR2608@users.noreply.github.com>
2021-08-02 23:57:04 +0100

    Add basic frontend with grid with variable size

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import HomePage from "./components/HomePage";
+import GridPage from "./components/GridPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link className="navbar-brand" to="/">
+            CherryLite
+          </Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr_auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/grid" className="nav-link">
+                  Grid
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/:gridId" exact component={GridPage} />
+      </div>
+    </Router>
+  );
+}
+export default App;
